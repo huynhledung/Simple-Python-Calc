a9a0f446186d974fc9584e7598bb832627035554
DzungHuynh <huynhledung77@gmail.com>
2017-10-10 16:53:51 +1100

    Allow Decemals

--- calc.py
+++ calc.py
@@ -1,8 +1,11 @@
 calc.py: A simple command
 import sys
-filename, num1, op, num2 =sys.argv num1, num2 = int(num1), int (num2)
+filename, num1, op, num2 =sys.argv num1, num2 = float(num1), float (num2)
+
 if op == '+': #only support addition now print(num1, num2)
 elif op == '-': print (num1-num2)
 elif op == "*": print(num1*num2)
+elif op == "^": print(num1*num1)
 else: print"0"
-#end of file
+#end of file:
+
